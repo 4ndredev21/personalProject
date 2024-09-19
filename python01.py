8be32df52a450a1724fc6472a96d7a27033e2622
4ndredev21 <4ndredev21@gmail.com>
2024-09-19 16:31:10 -0300

    adicionado sobre 'imput' em python

---
+++ python01.py
@@ -0,0 +1,15 @@
+# Como esperado, o Python acertou todos os tipos de variáveis. Note, também, que em Python tudo é objeto. Sendo assim, os tipos de dados aparecem com a palavra “class”, que é uma classe.
+
+# Já conseguimos criar uma variável. Que tal melhorar o famoso “hello world”? Para isso, usaremos a função input(), que faz a leitura de um valor digitado.
+
+nome = input('Digite aqui:')
+
+print(nome)
+
+# Digite um nome: Estudante Querido
+
+# Estudante Querido
+
+# Ao executar o comando input, surge o campo para digitar o que será capturado – no nosso caso, “Estudante Querido”. Logo após, a função print() mostra a variável “nome”. Vamos melhorar nosso “hello world”!
+
+# Existem muitas formas de imprimir textos e variáveis em Python. Usaremos formatadores de caracteres (igual em C) e a f-string.
