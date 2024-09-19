9ff05d829d4d0f51178f128f516cdb686b82d6a1
4ndredev21 <4ndredev21@gmail.com>
2024-09-19 16:19:08 -0300

    adicionado aula de python

--- python.py
+++ python.py
@@ -1,1 +1,13 @@
-print("hello world")
+# Variáveis e tipos de dados
+# O fluxo de um algoritmo é a entrada, o processamento e a saída. Note que, para que o processamento ocorra, é necessário armazenar os valores da entrada, por exemplo. Assim surge o conceito de variável, que nada mais é do que um espaço alocado na memória RAM.
+
+# O interpretador Python consegue estabelecer o tipo de dado da variável observando seu valor. Confira alguns exemplos:
+
+x = 10
+
+nome = 'aluno'
+
+nota = 8.75
+
+fez_inscricao = True
+
