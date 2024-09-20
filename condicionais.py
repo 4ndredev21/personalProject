02b36e4efc6027d20d23540de937943f1247ad20
4ndredev21 <4ndredev21@gmail.com>
2024-09-20 12:00:42 -0300

    arrumando o código

--- condicionais.py
+++ condicionais.py
@@ -6,3 +6,9 @@ elif idade >= 18 and idade <= 65 :
     print(f"sua idade é de : {idade} , então você é um adulto")
 else :
     print(f"sua idade é de : {idade}, então você é um idoso")
+    
+idade_certa = int(idade)
+
+if idade_certa == int(66) :
+    idade_certa = input("qual a sua idade?") :
+        if idade_certa 
